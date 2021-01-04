b574adab77a0709f505ec98375803af65757e70b
Antonio Ruggiero <ant.ruggiero16@gmail.com>
2021-01-04 22:48:34 +0100

    fixes on personal info view

--- lib/views/personal_info_view.dart
+++ lib/views/personal_info_view.dart
@@ -1,9 +1,9 @@
 import 'dart:html';
 
 import 'package:AntonioRuggiero/custom_icons_icons.dart';
+import 'package:AntonioRuggiero/views/widgets/PersonalContactListItem.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
-import 'package:url_launcher/url_launcher.dart';
 
 class PersonalInfoView extends StatelessWidget {
   @override
@@ -11,73 +11,51 @@ class PersonalInfoView extends StatelessWidget {
     return Container(
       padding: EdgeInsets.all(32),
       child: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        child: Stack(
           children: <Widget>[
-            Column(children: [
-              Text(
-                "Antonio Ruggiero",
-                style: TextStyle(fontSize: 35, fontWeight: FontWeight.bold),
-              ),
-              const SizedBox(
-                height: 16,
-              ),
-              Text(
-                "Hello! I'm Antonio Ruggiero, i came from Italy and actually i'm a XR developer @ Uqido, Padova",
-                style: TextStyle(fontSize: 18),
-              ),
-              const SizedBox(
-                height: 12,
-              ),
-              Text(
-                "This Website serve as personal portfolio, feel free to explore it or look at source code on github",
-                style: TextStyle(fontSize: 18),
-              ),
-            ]),
-            Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              mainAxisSize: MainAxisSize.min,
-              children: [
-                Text(
-                  "Contacts",
-                  style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
-                ),
-                const SizedBox(
-                  height: 8,
-                ),
-                ListTile(
-                  leading: Icon(
-                    Icons.email,
-                    size: 16,
+            Align(
+                alignment: Alignment.topCenter,
+                child: Column(children: [
+                  Text(
+                    "Antonio Ruggiero",
+                    style: TextStyle(fontSize: 35, fontWeight: FontWeight.bold),
                   ),
-                  title: Text("Email"),
-                  onTap: () => launch("mailto:ant.ruggiero16@gmail.com"),
-                ),
-                const SizedBox(
-                  height: 4,
-                ),
-                ListTile(
-                  leading: Icon(
-                    CustomIcons.instagram,
-                    size: 16,
+                  const SizedBox(
+                    height: 16,
                   ),
-                  title: Text("Instagram Profile"),
-                  onTap: () =>
-                      launch("https://www.instagram.com/antonio.ruggiero93/"),
-                ),
-                const SizedBox(
-                  height: 4,
-                ),
-                ListTile(
-                  leading: Icon(
-                    CustomIcons.github,
-                    size: 16,
+                  Text(
+                    "Hello! I'm Antonio Ruggiero, i came from Italy and actually i'm a XR developer @ Uqido, Padova",
+                    style: TextStyle(fontSize: 18),
                   ),
-                  title: Text("Github Profile"),
-                  onTap: () => launch("https://github.com/BattlefieldNoob"),
-                ),
-              ],
-            )
+                  const SizedBox(
+                    height: 12,
+                  ),
+                  Text(
+                    "This Website serve as personal portfolio, feel free to explore it or look at source code on github",
+                    style: TextStyle(fontSize: 18),
+                  ),
+                ])),
+            Align(
+                alignment: Alignment.bottomLeft,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    Text(
+                      "Contacts",
+                      style:
+                          TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+                    ),
+                    PersonalContactListItem(Icons.email, "email",
+                        "mailto:ant.ruggiero16@gmail.com"),
+                    PersonalContactListItem(
+                        CustomIcons.instagram,
+                        "Instagram Profile",
+                        "https://www.instagram.com/antonio.ruggiero93/"),
+                    PersonalContactListItem(CustomIcons.github,
+                        "Github Profile", "https://github.com/BattlefieldNoob"),
+                  ],
+                ))
           ],
         ),
       ),
---
+++ lib/views/widgets/PersonalContactListItem.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:url_launcher/url_launcher.dart';
+
+class PersonalContactListItem extends StatelessWidget {
+  final IconData icon;
+  final String label;
+  final String url;
+
+  const PersonalContactListItem(
+    this.icon,
+    this.label,
+    this.url,
+  ) : super(key: null);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+        padding: EdgeInsets.all(4),
+        child: TextButton.icon(
+          icon: Icon(
+            icon,
+            size: 16,
+          ),
+          label: Text(label),
+          onPressed: () => launch(url),
+        ));
+  }
+}
